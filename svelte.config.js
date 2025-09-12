debe09471d331528a24dad09299ad3c2c9c4db18
brie 🍦 <nataliusgabriel@gmail.com>
2025-09-12 10:22:18 +0700

    svelte config update

--- svelte.config.js
+++ svelte.config.js
@@ -15,6 +15,9 @@ const config = {
 			precompress: false,
 			strict: true
 		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 		// csp: {
 		// 	directives: {
 		// 		'style-src': ['https://accounts.google.com/gsi/style', 'https://fonts.googleapis.com'],
