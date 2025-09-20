69c47f91c4e26acd16842ae9be027c2f3c13c28d
brie 🍦 <nataliusgabriel@gmail.com>
2025-09-20 12:44:21 +0700

    email with log

--- src/lib/user.svelte.js
+++ src/lib/user.svelte.js
@@ -53,7 +53,7 @@ const request = async (endpoint, body) => {
             alert(err.response.data)
         } else {
             await axios.post(API_URI+"/log", {
-                "message": err.toString()
+                "message": userState.email+" - "+err.toString()
             })
             console.error(err)
             alert(err)
